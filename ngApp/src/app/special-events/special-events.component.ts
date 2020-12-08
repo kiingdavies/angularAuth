a34e4dcb02dad7ef69ed3515b43979e1c46d2e30
kiingdavies <daviesadedeji007@gmail.com>
2020-12-08 16:13:22 +0100

    Middleware to verify jsonwebtoken done

--- ngApp/src/app/special-events/special-events.component.ts
+++ ngApp/src/app/special-events/special-events.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './../event.service';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-special-events',
@@ -10,12 +12,18 @@ export class SpecialEventsComponent implements OnInit {
   // create an empty array to hold the returned specila events
   specialEvents = [];
 
-  constructor(private _eventService: EventService) {}
+  constructor(private _eventService: EventService, private _router: Router) {}
 
   ngOnInit(): void {
     this._eventService.getSpecialEvents().subscribe(
       (res) => (this.specialEvents = res),
-      (err) => console.log(err)
+      (err) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status === 401) {
+            this._router.navigate(['/login']);
+          }
+        }
+      }
     );
   }
 }
--- server/routes/api.js
+++ server/routes/api.js
@@ -15,6 +15,23 @@ mongoose.connect(db,{ useNewUrlParser: true, useUnifiedTopology: true },(err) =>
   }
 );
 
+// middleware to verify if the user's jsonwebtoken is in thebackend
+function verifyToken(req, res, next){
+  if (!req.headers.authorization){
+    return res.status(401).send('Unauthorized request')
+  }
+  let token = req.headers.authorization.split(' ')[1] // this splits the Bearer string out and returns only the token
+  if (token === 'null'){
+    return res.status(401).send('Unauthorized request')
+  }
+  let payload = jwt.verify(token, 'secretkey')
+  if(!payload){
+    return res.status(401).send('Unauthorized request')
+  }
+  req.userId = payload.subject
+  next()
+}
+
 // this uses a get Method to send requests from frontend to the endpoint localhost:3000/api
 router.get("/", (req, res) => {
   res.send("Called From api.js route");
@@ -100,7 +117,7 @@ router.get("/events", (req, res) => {
 });
 
 // this uses a get Method to send requests from frontend to the endpoint localhost:3000/api/special
-router.get("/special", (req, res) => {
+router.get("/special", verifyToken, (req, res) => {
   // normally the list of special should be calling the data from mongodb using findAll method but this is test
   let events = [
     {
