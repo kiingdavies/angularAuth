5066da2857a6884c5dd082da431233eb76ab7046
kiingdavies <daviesadedeji007@gmail.com>
2020-12-08 12:39:53 +0100

    localstorage & router navigation done

--- ngApp/src/app/register/register.component.ts
+++ ngApp/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from './../auth.service';
 
 @Component({
@@ -10,15 +11,20 @@ export class RegisterComponent implements OnInit {
   // registerUserData is the object containung the email and password of the user about to be registered
   registerUserData = {
     email: '',
-    password: ''
+    password: '',
   };
-  constructor(private _auth: AuthService) {}
+  constructor(private _auth: AuthService, private _router: Router) {}
 
   ngOnInit(): void {}
 
+  // function to register new user when called
   registerUser() {
     this._auth.registerUser(this.registerUserData).subscribe(
-      (res) => console.log(res),
+      (res) => {
+        // console.log(res)
+        localStorage.setItem('token', res.token);
+        this._router.navigate(['/special']); // this navigates us to special events route when registered successfully
+      },
       (err) => console.log(err)
     );
   }
